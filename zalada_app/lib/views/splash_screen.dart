cdedd71d1dcb1b051e9bf16417fc93d85d63d347
vativeApps <vativeapps@MacBook-Pro.local>
2023-07-20 10:53:09 +0500

    init commit

---
+++ zalada_app/lib/views/splash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class splash_screen extends StatelessWidget {
+  const splash_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

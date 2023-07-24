6cfc93cc8f2bb5aca52ea8bd49147cae1d12ed18
Zaryab Alam <88553537+ZaryabAlam@users.noreply.github.com>
2023-07-24 16:39:13 +0500

    4:40 kamlesh update 24/07/23

--- zalada_app/lib/views/home_screen.dart
+++ zalada_app/lib/views/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:zalada_app/custom/product_card.dart';
 
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:zalada_app/utiles/page_navigation.dart';
+import 'package:zalada_app/views/notification_screen.dart';
 import 'package:zalada_app/views/product_detail.dart';
 import '../custom/hot_deal_productcard.dart';
 
@@ -57,14 +58,20 @@ class Home_Screen extends StatelessWidget {
                               .withOpacity(0.3)),
                       child: Image.asset('assets/images/favorite.png'),
                     ),
-                    Container(
-                      margin: EdgeInsets.all(7),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Theme.of(context)
-                              .highlightColor
-                              .withOpacity(0.3)),
-                      child: Image.asset('assets/images/notification.png'),
+                    InkWell(
+                      onTap: () {
+                        Page_Navigation().Page_ReplaceNavigation(
+                            context, Notification_Screen());
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Theme.of(context)
+                                .highlightColor
+                                .withOpacity(0.3)),
+                        child: Image.asset('assets/images/notification.png'),
+                      ),
                     ),
                   ],
                   leading: ClipRRect(
---
+++ zalada_app/lib/views/notification_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:zalada_app/custom/back_button.dart';
+
+class Notification_Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final hieght = MediaQuery.of(context).size.height;
+    final ph = 20.0;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Theme.of(context).secondaryHeaderColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: back_button(ontap: () {
+          Get.back();
+        }),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'your_notification'.tr,
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).hintColor),
+                ),
+                Text(
+                  'today'.tr,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).hintColor),
+                ).py(15),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Container(
+                    margin: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color:
+                            Theme.of(context).disabledColor.withOpacity(0.2)),
+                    child: Image.asset(
+                      'assets/images/cart.png',
+                      height: 30,
+                      color: Vx.red800,
+                    ).p(12),
+                  ),
+                  title: Text(
+                    '30% Special Discount!',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  subtitle: Text('Special promo to all apple device'),
+                ),
+                Divider(
+                  color: Theme.of(context).disabledColor.withOpacity(0.5),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Container(
+                    margin: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color:
+                            Theme.of(context).disabledColor.withOpacity(0.2)),
+                    child: Image.asset(
+                      'assets/images/cart.png',
+                      height: 30,
+                      color: Vx.red800,
+                    ).p(12),
+                  ),
+                  title: Text(
+                    '30% Special Discount!',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  subtitle: Text('Special promo to all apple device'),
+                ),
+              ],
+            ).px(20),
+            Divider(
+              color: Theme.of(context).disabledColor.withOpacity(0.2),
+              thickness: 3,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'yesterday'.tr,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).hintColor),
+                ).py(15),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Container(
+                    margin: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color:
+                            Theme.of(context).disabledColor.withOpacity(0.2)),
+                    child: Image.asset(
+                      'assets/images/cart.png',
+                      height: 30,
+                      color: Vx.red800,
+                    ).p(12),
+                  ),
+                  title: Text(
+                    '30% Special Discount!',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  subtitle: Text('Special promo to all apple device'),
+                ),
+                Divider(
+                  color: Theme.of(context).disabledColor.withOpacity(0.5),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Container(
+                    margin: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color:
+                            Theme.of(context).disabledColor.withOpacity(0.2)),
+                    child: Image.asset(
+                      'assets/images/cart.png',
+                      height: 30,
+                      color: Vx.red800,
+                    ).p(12),
+                  ),
+                  title: Text(
+                    '30% Special Discount!',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  subtitle: Text('Special promo to all apple device'),
+                ),
+                Divider(
+                  color: Theme.of(context).disabledColor.withOpacity(0.5),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Container(
+                    margin: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color:
+                            Theme.of(context).disabledColor.withOpacity(0.2)),
+                    child: Image.asset(
+                      'assets/images/cart.png',
+                      height: 30,
+                      color: Vx.red800,
+                    ).p(12),
+                  ),
+                  title: Text(
+                    '30% Special Discount!',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  subtitle: Text('Special promo to all apple device'),
+                ),
+              ],
+            ).px(20),
+          ],
+        ),
+      ),
+    );
+  }
+}
